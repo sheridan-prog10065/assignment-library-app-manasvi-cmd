160853610592ddc6c90dd45e4a7f81b238257765
Manasvi reddy kayiti <Kayiti@sheridancollege.ca>
2026-04-19 20:02:44 -0400

    Designed Library Admin pafe UI

--- LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryAdminPage.xaml.cs
+++ LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryAdminPage.xaml.cs
@@ -11,5 +11,11 @@ public partial class LibraryAdminPage : ContentPage
     public LibraryAdminPage()
     {        
         InitializeComponent();
+        GenrePicker.ItemsSource = Enum.GetNames(typeof(Genre)).ToList();
+    }
+
+    private void AddBookButton_Clicked(object sender, EventArgs e)
+    {
+        AdminMessageLabel.Text = "Book registration button clicked.";
     }
 }
